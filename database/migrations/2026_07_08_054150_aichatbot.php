ab87b2643d9e36940b883ae0d36e08aef8e77393
ikechukwu <onyenoikechukwu08006@gmail.com>
2026-07-12 13:53:00 +0100

    correction done to migration

--- database/migrations/2026_07_08_054150_aichatbot.php
+++ database/migrations/2026_07_08_054150_aichatbot.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('chat', function(Blueprint $table){
             $table->id();
-            $table->VARCHAR('user_id')
+            $table->foreignId('user_id')
                 ->nullable()
                 ->constrained()
                 ->cascadeOnDelete();
