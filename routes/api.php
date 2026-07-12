8a119e6c4aef5228529a77be631a2b0f28b8629e
ikechukwu <onyenoikechukwu08006@gmail.com>
2026-07-12 14:26:21 +0100

    added the routes

--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,20 @@ Route::middleware(['auth:sanctum', 'throttle:6,1'])->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::apiResource('bot/chat', AiChatBotController::class);
-    Route::get(
-    'bot/payment/callback',
-    [FlutterwaveController::class,'callback']
-    );
-    Route::post('bot/payment', [FlutterwaveController::class, 'checkout']);
-
-    Route::get('/payment/callback', [FlutterwaveController::class, 'callback']);
+    Route::prefix('bot')->group(function(){
+        Route::apiResource('chat', AiChatBotController::class);
+        Route::get(
+        'payment/callback',
+        [FlutterwaveController::class,'callback']
+        );
+        Route::post('payment', [FlutterwaveController::class, 'checkout']);
+    });
 }); 
 
-Route::apiResource('botunregistered-chat/chat', UnRegisteredAiChatBotController::class);
+Route::prefix('bot/botunregistered-chat')->group(function () {
+    Route::apiResource('chat', UnRegisteredAiChatBotController::class);
+});
+
 
 
 require __DIR__.'/auth.php';
